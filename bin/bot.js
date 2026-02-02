50d600d6f892d771a8668ac5cb57f876ec883fce
Srdjan S <stankovic.srdjo@gmail.com>
2026-02-02 17:29:05 +0100

    chore: perform swap with bid in parallel

--- bin/bot.js
+++ bin/bot.js
@@ -1,14 +1,36 @@
 import 'dotenv/config'
 import { setTimeout } from 'node:timers/promises'
-import { initializeConfig, processAuctions } from '../index.js'
+import { discoverSushiswapRouter } from '../lib/swap.js'
+import { initializeConfig, ensureApproval } from '../lib/config.js'
+import { processAuctions } from '../lib/auction.js'
 
 const config = await initializeConfig(process.env)
+const sushiswapRouterAddress =
+  config.chainId === 314
+    ? await discoverSushiswapRouter(config.usdfcAddress, config.walletAddress)
+    : null
+
+if (sushiswapRouterAddress) {
+  await ensureApproval({
+    publicClient: config.publicClient,
+    walletClient: config.walletClient,
+    account: config.account,
+    tokenAddress: config.usdfcAddress,
+    spenderAddress: /** @type {import('viem').Address} */ (
+      sushiswapRouterAddress
+    ),
+  })
+}
+
+console.log()
+console.log('Starting auction monitoring...')
+console.log()
 
 while (true) {
   console.log(`Starting auction check...`)
 
   try {
-    await processAuctions(config)
+    await processAuctions({ ...config, sushiswapRouterAddress })
   } catch (error) {
     const err = /** @type {Error} */ (error)
     console.log()
